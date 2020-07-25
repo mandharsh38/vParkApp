a9834831c7b0f2c048b461d0ba4b750ec4e03b58
mandharsh38 <62983544+mandharsh38@users.noreply.github.com>
2020-07-25 12:32:18 +0530

    Updated a lot of stuff

---
+++ database.py
@@ -0,0 +1,57 @@
+import datetime
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+
+class DataBase:
+    def __init__(self, filename):
+        self.filename = filename
+        self.users = None
+        self.file = None
+        self.load()
+
+    def load(self):
+        self.file = open(self.filename, "r")
+        self.users = {}
+
+        for line in self.file:
+            email, password, name, created = line.strip().split(";")
+            self.users[email] = (password, name, created)
+
+        self.file.close()
+
+    def get_user(self, email):
+        if email in self.users:
+            return self.users[email]
+        else:
+            return -1
+
+    def add_user(self, email, password, name):
+        if email.strip() not in self.users:
+            self.users[email.strip()] = (password.strip(), name.strip(), DataBase.get_date())
+            self.save()
+
+        else:
+            self.invalidSubmit()
+
+
+    def invalidSubmit(self):
+        pop = Popup(title='Invalid Login',
+                    content=Label(text='Email already exists'),
+                    size_hint=(None, None), size=(220, 220))
+        pop.open()
+
+    def validate(self, email, password):
+        if self.get_user(email) != -1:
+            return self.users[email][0] == password
+        else:
+            return False
+
+    def save(self):
+        with open(self.filename, "w") as f:
+            for user in self.users:
+                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+
+    @staticmethod
+    def get_date():
+        return str(datetime.datetime.now()).split(" ")[0]
+
